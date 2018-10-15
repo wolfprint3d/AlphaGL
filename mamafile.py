c0edb80b65b218b09e4ee113687678a20e3c8ed3
Jorma <jorma.rebane@gmail.com>
2018-10-15 19:37:38 +0300

    Updated mamafile for Windows

--- mamafile.py
+++ mamafile.py
@@ -2,10 +2,13 @@ import mama
 class AlphaGL(mama.BuildTarget):
     local_workspace = 'build'
     def dependencies(self):
+        if self.linux:
+            self.add_git('libpng', 'https://github.com/glennrp/libpng.git', mamafile='mama/libpng.py')
+        else:
+            self.add_git('libpng', 'https://github.com/LuaDist/libpng.git', mamafile='mama/libpng.py')
+        self.add_git('libjpeg', 'https://github.com/LuaDist/libjpeg.git', mamafile='mama/libjpeg.py')
+        self.add_git('glfw',    'https://github.com/glfw/glfw.git', mamafile='mama/glfw.py')
         self.add_git('ReCpp',   'https://github.com/RedFox20/ReCpp.git')
-        self.add_git('libpng',  'https://github.com/glennrp/libpng.git')
-        self.add_git('libjpeg', 'https://github.com/LuaDist/libjpeg.git')
-        self.add_git('glfw',    'https://github.com/glfw/glfw.git')
 
     def package(self):
         self.export_libs('.', ['.lib', '.a']) # export any .lib or .a from build folder
