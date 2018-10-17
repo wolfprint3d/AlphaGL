584b0634745f94a0e8950167781ee895d4f0eb7a
Jorma <jorma.rebane@gmail.com>
2018-10-17 20:48:29 +0300

    Updated libpng ref

--- mama/libpng.py
+++ mama/libpng.py
@@ -8,10 +8,10 @@ class libpng(mama.BuildTarget):
         zinclude, zlibrary = self.get_target_products('zlib')
         self.add_cmake_options(f'ZLIB_INCLUDE_DIR={zinclude}')
         self.add_cmake_options(f'ZLIB_LIBRARY={zlibrary}')
-        self.add_cmake_options('PNG_STATIC=ON',
-                               'PNG_SHARED=OFF',
-                               'PNG_TESTS=OFF',
-                               'PNG_DEBUG=OFF')
+        self.add_cmake_options('PNG_STATIC=YES',
+                               'PNG_SHARED=NO',
+                               'PNG_TESTS=NO',
+                               'PNG_DEBUG=NO')
 
     def package(self):
         self.export_libs('lib', ['.lib', 'libpng16.a'])
--- mamafile.py
+++ mamafile.py
@@ -5,7 +5,7 @@ class AlphaGL(mama.BuildTarget):
         if self.linux:
             self.add_git('libpng', 'https://github.com/glennrp/libpng.git', mamafile='mama/libpng.py')
         else:
-            self.add_git('libpng', 'https://github.com/LuaDist/libpng.git', mamafile='mama/libpng.py')
+            self.add_git('libpng', 'https://github.com/RedFox20/libpng.git', mamafile='mama/libpng.py')
         self.add_git('libjpeg', 'https://github.com/LuaDist/libjpeg.git', mamafile='mama/libjpeg.py')
         self.add_git('glfw',    'https://github.com/glfw/glfw.git', mamafile='mama/glfw.py')
         self.add_git('ReCpp',   'https://github.com/RedFox20/ReCpp.git')
